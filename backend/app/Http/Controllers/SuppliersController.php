0e49893f5a20a8ff3294c08849d1a9a409f63ffa
David Manjarres <dmanjarres6065@uta.edu.ec>
2024-12-06 11:41:43 -0500

    Suppliers api

---
+++ backend/app/Http/Controllers/SuppliersController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SupplierRequest;
+use App\Models\Supplier;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use \Illuminate\Validation\ValidationException;
+
+class SuppliersController extends Controller
+{
+    public function index(){
+        $suppliers = Supplier::all();
+        
+        return response()->json([
+            'results' => $suppliers,
+            'message' => 'Operación exitosa',
+        ], 200);
+    }
+    
+    public function show($id){
+        try {
+            $supplier = Supplier::findOrFail($id);
+            
+            return response()->json([
+                'results' => $supplier,
+                'message' => 'Operación exitosa',
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Proveedor no encontrado',
+                'error' => $e->getMessage(),
+            ], 404);
+        }
+    }
+
+    public function store(SupplierRequest $request)
+    {
+        try {
+            $validated = $request->validated();  
+            $supplier = Supplier::create([
+                'nam_sup' => $validated['name'],
+                'ema_sup' => $validated['email'],
+                'pho_sup' => $validated['phone'],
+            ]);
+    
+            return response()->json([
+                'message' => 'Proveedor creado con éxito',
+                'results' => $supplier,
+            ], 201); 
+    
+        } catch (ValidationException $e) {
+            $errors = $e->errors();
+            if (isset($errors['email']) && strpos($errors['email'][0], 'unique') !== false) {
+                return response()->json([
+                    'message' => 'El correo electrónico ya está registrado.',
+                ], 409); 
+            }
+            return response()->json([
+                'message' => 'Datos no válidos',
+                'errors' => $errors,
+            ], 422);
+    
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Hubo un error al crear el proveedor.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function update(SupplierRequest $request, $id){
+        $supplier = Supplier::findOrFail($id);
+
+        $validated = $request->validated();
+        $supplier->update([
+            'nam_sup' => $validated['name'],
+            'ema_sup' => $validated['email'],
+            'pho_sup' => $validated['phone'],
+        ]);
+
+        return response()->json([
+            'message' => 'Proveedor actualizado con éxito',
+            'results' => $supplier,
+        ], 200);
+    }
+
+    public function destroy($id){
+        // Buscar el proveedor y manejar errores si no existe
+        try {
+            $supplier = Supplier::findOrFail($id);
+            $supplier->delete();
+
+            return response()->json([
+                'message' => 'Proveedor eliminado con éxito',
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Proveedor no encontrado',
+                'error' => $e->getMessage(),
+            ], 404);
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,11 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTAuthController;
+use App\Http\Controllers\SuppliersController;
+
 use App\Http\Middleware\JwtMiddleware;
 
 // Coloca aqui las rutas que no estarán protegidas.
 Route::post('register', [JWTAuthController::class, 'register']);
 Route::post('login', [JWTAuthController::class, 'login']);
+
+
+Route::get('suppliers', [SuppliersController::class, 'index']);
+Route::get('suppliers/{id}', [SuppliersController::class, 'show']);
+Route::post('suppliers', [SuppliersController::class, 'store']);
+Route::put('suppliers/{id}', [SuppliersController::class, 'update']);
+Route::delete('suppliers/{id}', [SuppliersController::class, 'destroy']);
+
 
 Route::middleware([JwtMiddleware::class])->group(function () {
     // Coloca aqui las rutas que estarán protegidas.
