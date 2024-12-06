a43583e076d38c954d70f27ab140a94752aa46c4
Joel Bonilla <bjeferssonvinicio2005@gmail.com>
2024-12-06 15:12:43 -0500

    Arreglando  nav

--- frontend/src/components/theme.jsx
+++ frontend/src/components/theme.jsx
@@ -1,17 +1,54 @@
 import { createTheme } from "@mui/material/styles";
 
 export const demoTheme = createTheme({
-  cssVariables: {
-    colorSchemeSelector: "data-toolpad-color-scheme",
+  palette: {
+    primary: {
+      main: "#424874",
+    },
+    secondary: {
+      main: "#7986CB",
+    },
   },
-  colorSchemes: { light: true, dark: true },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 600,
-      lg: 1200,
-      xl: 1536,
+  components: {
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: "#424874",
+          color: "#FFFFFF",
+        },
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#6068a5",
+          color: "#FFFFFF",
+        },
+      },
+    },
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          color: "white",
+        },
+      },
+    },
+    MuiListItem: {
+      styleOverrides: {
+        root: {
+          "&.Mui-selected": {
+            backgroundColor: "#424874",
+            color: "white",
+          },
+        },
+      },
+    },
+    MuiListItemText: {
+      styleOverrides: {
+        root: {
+          color: "white",
+        },
+      },
     },
   },
 });
