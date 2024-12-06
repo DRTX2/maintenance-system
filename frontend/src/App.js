995fb4f2c3475e5fd5eadb105e4b0d61f5e9ef85
David Manjarres <dmanjarres6065@uta.edu.ec>
2024-12-06 15:12:48 -0500

    Adding Supplier page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard"; // Importa el componente Dashboard
+import Dashboard from "./pages/Dashboard"; 
+import Suppliers from "./pages/Suppliers";
 
 function Prueba() {
   return "Hola";
@@ -14,7 +15,7 @@ function App() {
         {/* Puedes agregar más rutas si es necesario */}
         <Route path="/prueba" element={<Prueba />} />{" "}
         {/* Ruta para Dashboard */}
-        
+        <Route path="/suppliers" element={<Suppliers/>}/>
       </Routes>
     </Router>
   );
