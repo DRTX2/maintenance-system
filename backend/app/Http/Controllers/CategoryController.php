193ea4b2e48bb306fed2d5757a4928632aed25d6
Josué García <josuegarcab2@hotmail.com>
2024-12-06 13:34:07 -0500

    New files

---
+++ backend/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Category;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        $categories = Category::all();
+
+        return response()->json([
+            'results' => $categories,
+            'message' => 'Categoria obtenida con exito.'
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Validación de los datos del request
+        $request->validate([
+            'cod_dis' => 'required|string|max:10|unique:categories,cod_dis',
+            'tip_dis' => 'required|string|max:10',
+            'nom_dis' => 'required|string|max:25'
+        ]);
+
+        try {
+            // Intentamos crear el registro
+            $category = Category::create($request->all());
+
+            // Devolvemos la respuesta
+            return response()->json([
+                'category' => $category,
+                'message' => 'Categoría almacenada correctamente'
+            ], 201);
+
+        } catch (\Exception $e) {
+            // Devolvemos el error
+            return response()->json([
+                'Error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json(['message' => 'Categoria no encontrada'], 404);
+        }
+
+        return response()->json([
+            'result' => $category,
+            'message' => 'Categoria obtenida con exito.'
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        // Buscar la categoría a actualizar
+        $category = Category::find($id);
+
+        // Si no se encuentra la categoría
+        if (!$category) {
+            return response()->json(['message' => 'Categoría no encontrada'], 404);
+        }
+
+        // Validación de los campos
+        $request->validate([
+            'cod_dis' => 'required|unique:categories,cod_dis,' . $category->id,
+            'tip_dis' => 'required|string|max:20',
+            'nom_dis' => 'required|string|max:25',
+        ]);
+
+        // Actualizar la categoría con los datos validados
+        $category->update($request->all());
+
+        // Responder con la categoría actualizada
+        return response()->json(['message' => 'Categoría actualizada', 'data' => $category], 200);
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json(['message' => 'Categoria no encontrada'], 404);
+        }
+
+        try {
+            $category->delete();
+            return response()->json(['message' => 'Categoria eliminada exitosamente'], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error al eliminar la categoría'], 500);
+        }
+    }
+}
---
+++ backend/routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LocationController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JWTAuthController;
+use App\Http\Controllers\SuppliersController;
+
+use App\Http\Middleware\JwtMiddleware;
+
+// Coloca aqui las rutas que no estarán protegidas.
+Route::post('register', [JWTAuthController::class, 'register']);
+Route::post('login', [JWTAuthController::class, 'login']);
+
+
+Route::get('suppliers', [SuppliersController::class, 'index']);
+Route::get('suppliers/{id}', [SuppliersController::class, 'show']);
+Route::post('suppliers', [SuppliersController::class, 'store']);
+Route::put('suppliers/{id}', [SuppliersController::class, 'update']);
+Route::delete('suppliers/{id}', [SuppliersController::class, 'destroy']);
+
+Route::get('category', [CategoryController::class, 'index']);
+Route::post('category/store', [CategoryController::class, 'store']);
+Route::get('category/show/{id}', [CategoryController::class, 'show']);
+Route::put('category/update/{id}', [CategoryController::class, 'update']);
+Route::delete('category/destroy/{id}', [CategoryController::class, 'destroy']);
+
+Route::middleware([JwtMiddleware::class])->group(function () {
+    // Coloca aqui las rutas que estarán protegidas.
+});
+
+//Rutas para las ubicaciones 
+
+Route::get('/locations', [LocationController::class, 'index']);
+Route::post('/locations', [LocationController::class, 'store']);
+Route::get('/locations/{id}', [LocationController::class, 'show']);
+Route::put('/locations/{id}', [LocationController::class, 'update']);
+Route::delete('/locations/{id}', [LocationController::class, 'destroy']);
