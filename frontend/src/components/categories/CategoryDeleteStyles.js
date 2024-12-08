36d3b5c0c2eb8f5adefb6fc554c6aa09ebbce31d
Josué García <josuegarcab2@hotmail.com>
2024-12-08 12:19:37 -0500

    Fixing a style

--- frontend/src/components/categories/CategoryDeleteStyles.js
+++ frontend/src/components/categories/CategoryDeleteStyles.js
@@ -3,12 +3,16 @@ const categoryDeleteStyles = {
     marginLeft: "5px",
     marginRight: "5px",
     marginBottom: "10px",
+    borderRadius: "0px",
+    padding: "2px 20px",
   },
   buttonStyle2: {
     marginLeft: "5px",
     marginRight: "5px",
     marginBottom: "10px",
     backgroundColor: "#AE445A",
+    borderRadius: "0px",
+    padding: "2px 20px",
   },
 };
 
