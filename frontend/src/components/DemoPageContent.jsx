fcfe85d71db675dbf7b76a75bfd9db4e6947ef91
David Manjarres <dmanjarres6065@uta.edu.ec>
2024-12-11 09:28:22 -0500

    Prev Changes

--- frontend/src/components/DemoPageContent.jsx
+++ frontend/src/components/DemoPageContent.jsx
@@ -4,12 +4,13 @@ import Box from "@mui/material/Box";
 import SuplierMaganer from "../generic/implementations/SuplierManager";
 import LocationManager from "../generic/implementations/LocationManager";
 import CategoryManager from "../generic/implementations/CategoryManager";
+import 
 
 function DemoPageContent({ pathname }) {
   const renderContent = (pathname) => {
     switch (pathname) {
       case "/usuarios":
-        return <div>Contenido de Usuarios</div>;
+        return ;
       case "/proveedores":
         return <SuplierMaganer />;
       case "/ubicaciones":
