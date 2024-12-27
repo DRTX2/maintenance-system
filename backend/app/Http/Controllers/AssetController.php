110cbffb84e85bfb2ae789deb54d0d4dc730191e
Joel Bonilla <bjeferssonvinicio2005@gmail.com>
2024-12-27 10:18:15 -0500

    Adding methods to update, hide, view and update index method in the assets section.

--- backend/app/Http/Controllers/AssetController.php
+++ backend/app/Http/Controllers/AssetController.php
@@ -4,43 +4,94 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AssetRequest;
 use App\Models\Asset;
+use App\Models\Income;
 use Illuminate\Http\Request;
 
 class AssetController extends Controller
 {
-    //Crear, Actualizar,Eliminar,Ver, Filtrar   
 
-
-    //cargar la tabla
-
-
-
-    public function index()
+    public function showOpenIncomes()
     {
 
-        $assets = Asset::with(['income', 'category', 'location'])->get();
 
+        $incomes = Income::where('est_inc', 'O')->get();
 
+        if ($incomes->isEmpty()) {
+            return response()->json([
+                'message' => 'No se encontraron ingresos'
+            ]);
+        }
+
+        return response()->json($incomes, 200);
+    }
+    public function hideAsset($id)
+    {
+        $asset = Asset::findOrFail($id);
+
+        if ($asset->est_ass === 'H') {
+            return response()->json([
+                'message' => 'El activo ya está oculto.',
+            ]);
+        }
+
+        $asset->update([
+            'est_ass' => 'H',
+        ]);
+
+        return response()->json([
+            'message' => 'Activo oculto.',
+        ]);
+    }
+
+    public function visibleAsset($id)
+    {
+        $asset = Asset::findOrFail($id);
+
+        if ($asset->est_ass === 'V') {
+            return response()->json([
+                'message' => 'El activo ya está visible.',
+            ]);
+        }
+
+        $asset->update([
+            'est_ass' => 'V',
+        ]);
+
+        return response()->json([
+            'message' => 'Activo visible.',
+        ]);
+    }
+
+    public function index($rol)
+    {
+
+        if ($rol == 'user') {
+            $assets = Asset::where('est_ass', 'V')
+                ->with(['income', 'category', 'location'])
+                ->get();
+
+        } else {
+
+            $assets = Asset::with(['income', 'category', 'location'])->get();
+        }
         if ($assets->isEmpty()) {
             return response()->json([
                 'message' => 'No se encontraron activos'
             ]);
         }
 
-
-        $transformedAssets = $assets->map(function ($asset) {
+        $transformedAssets = $assets->map(function ($asset) use ($rol) {
             return [
                 'id' => $asset->id,
-                'income_code' => $asset->income ? $asset->income->cod_inc : null,
-                'category_code' => $asset->category ? $asset->category->cod_dis : null,
-                'location_code' => $asset->location ? $asset->location->cod_loc : null,
+                'income_code' => $asset->income,
+                'category_code' => $asset->category,
+                'location_code' => $asset->location,
                 'cod_ass' => $asset->cod_ass,
+                'est_ass' => $rol === 'admin' ? $asset->est_ass : null,
                 'ser_num_ass' => $asset->ser_num_ass,
                 'obs_add_ass' => $asset->obs_add_ass ?? null,
             ];
         });
-
-
         return response()->json($transformedAssets, 200);
     }
 
@@ -48,78 +99,137 @@ class AssetController extends Controller
 
     public function show($id)
     {
-        // Cargar el activo junto con sus relaciones
-        $asset = Asset::with(['income', 'category', 'location', 'components'])->findOrFail($id);
 
-        // Transformar los componentes para incluir los datos relevantes y la información de la tabla pivote
+        $asset = Asset::with([
+            'income:id,cod_inc',
+            'category:id,cod_dis,nom_dis',
+            'location:id,cod_loc,nam_loc',
+            'components:id,cod_com,nam_com'
+        ])->findOrFail($id);
+
+        // Mapear los componentes para incluir solo los atributos deseados
         $components = $asset->components->map(function ($component) {
             return [
                 'id' => $component->id,
                 'cod_com' => $component->cod_com,
                 'nam_com' => $component->nam_com,
-                'des_com' => $component->des_com,
                 'pivot' => [
                     'description' => $component->pivot->description,
                 ],
             ];
         });
 
+
         $response = [
             'id' => $asset->id,
-            'id_inc_ass' => $asset->income ? $asset->income->id : null,
-            'id_cat_ass' => $asset->category ? $asset->category->id : null,
-            'id_loc_ass' => $asset->location ? $asset->location->id : null,
-            'income_code' => $asset->income ? $asset->income->cod_inc : null,
-            'category_code' => $asset->category ? $asset->category->cod_dis : null,
-            'location_code' => $asset->location ? $asset->location->cod_loc : null,
+            'id_inc_ass' => $asset->income->id,
+            'id_cat_ass' => $asset->category->id,
+            'id_loc_ass' => $asset->location->id,
+            'income_code' => $asset->income->cod_inc,
+            'category_code' => $asset->category->cod_dis,
+            'category_name' => $asset->category->nom_dis,
+            'location_code' => $asset->location->cod_loc,
+            'location_name' => $asset->location->nam_loc,
             'cod_ass' => $asset->cod_ass,
             'ser_num_ass' => $asset->ser_num_ass,
-            'obs_add_ass' => $asset->obs_add_ass,
+            'obs_add_ass' => $asset->obs_add_ass ?? null,
             'components' => $components,
         ];
-
 
         return response()->json($response);
     }
 
+
     public function store(AssetRequest $request)
     {
 
+        try {
+            $validatedData = $request->validated();
 
-        $validatedData = $request->validated();
-
-        $service = $validatedData['asset'];
-
-
-
-        $service = $request->input('asset');
-
-        $asset = Asset::create([
-
-            'id_inc_ass' => $service['id_inc_ass'],
-            'id_cat_ass' => $service['id_cat_ass'],
-            'id_loc_ass' => $service['id_loc_ass'],
-            'cod_ass' => $service['cod_ass'],
-            'ser_num_ass' => $service['ser_num_ass'],
-            'obs_add_ass' => $service['obs_add_ass'] ?? null,
-
-        ]);
+            $service = $validatedData['asset'];
 
 
-        $components = collect($service['components'])->mapWithKeys(function ($component) {
-            return [
-                $component['id'] => [
-                    'description' => $component['pivot']['description'],
-                ],
-            ];
-        });
 
-        $asset->components()->attach($components);
+            $service = $request->input('asset');
 
-        return response()->json([
-            'message' => 'Activo creado exitosamente.',
-            'asset' => $asset,
-        ]);
+            $asset = Asset::create([
+
+                'id_inc_ass' => $service['id_inc_ass'],
+                'id_cat_ass' => $service['id_cat_ass'],
+                'id_loc_ass' => $service['id_loc_ass'],
+                'cod_ass' => $service['cod_ass'],
+                'ser_num_ass' => $service['ser_num_ass'],
+                'obs_add_ass' => $service['obs_add_ass'] ?? null,
+
+            ]);
+
+
+            $components = collect($service['components'])->mapWithKeys(function ($component) {
+                return [
+                    $component['id'] => [
+                        'description' => $component['pivot']['description'],
+                    ],
+                ];
+            });
+
+            $asset->components()->attach($components);
+
+            return response()->json([
+                'message' => 'Activo creado exitosamente.',
+                'asset' => $asset,
+            ]);
+
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error al crear el activo:' . $e], 500);
+        }
+
+
+    }
+
+    public function update(AssetRequest $request, string $id)
+    {
+        try {
+
+            $validatedData = $request->validated();
+
+            $asset = Asset::findOrFail($id);
+
+            // Obtener los datos actualizados
+            $service = $validatedData['asset'];
+
+
+            $asset->update([
+                'id_inc_ass' => $service['id_inc_ass'],
+                'id_loc_ass' => $service['id_loc_ass'],
+                'cod_ass' => $service['cod_ass'],
+                'ser_num_ass' => $service['ser_num_ass'],
+                'obs_add_ass' => $service['obs_add_ass'] ?? $asset->obs_add_ass,
+            ]);
+
+            // Actualizar la relación con los componentes
+            if (isset($service['components'])) {
+                $components = collect($service['components'])->mapWithKeys(function ($component) {
+                    return [
+                        $component['id'] => [
+                            'description' => $component['pivot']['description'],
+                        ],
+                    ];
+                });
+
+                $asset->components()->sync($components);
+            }
+
+
+            return response()->json([
+                'message' => 'Activo actualizado exitosamente.',
+                'asset' => $asset->fresh(),
+            ]);
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error al actualizar el activo:' . $e], 500);
+        }
+
     }
 
 
