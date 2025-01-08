c47e8362dcea524fc2fddc29477189bd097a9b90
Josué García <josuegarcab2@hotmail.com>
2025-01-07 21:24:03 -0500

    Solved

---
+++ frontend/src/Loading.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h2>Cargando...</h2>
+    </div>
+  );
+};
+
+export default Loading;
