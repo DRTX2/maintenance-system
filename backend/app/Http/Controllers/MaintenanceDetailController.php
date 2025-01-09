c2086d34c20315d70b5d408a2cdafdcd4014900e
David Manjarres <dmanjarres6065@uta.edu.ec>
2025-01-09 16:43:49 -0500

    Fix maintenance detail store

--- backend/app/Http/Controllers/MaintenanceDetailController.php
+++ backend/app/Http/Controllers/MaintenanceDetailController.php
@@ -172,10 +172,28 @@ class MaintenanceDetailController extends Controller
             }
 
             DB::commit();
+            $formattedData = [
+                'id' => $maintenance->id,
+                'cod_main' => $maintenance->cod_main,
+                'vis_main' => $maintenance->vis_main,
+                'created_at' => $maintenance->created_at,
+                'ended_at' => $maintenance->ended_at,
+                'responsable' => $maintenance->responsible->nam_res . ' ' . $maintenance->responsible->las_res,
+                'type' => $maintenance->maintenanceType->typ_main,
+                'details' => $maintenance->details->map(function ($detail) {
+                    return [
+                        'id' => $detail->id,
+                        'asset_id' => $detail->id_ass_bel,
+                        'observations' => $detail->observations,
+                        'replaced_components' => $detail->replacedComponents,
+                        'activities' => $detail->activities,
+                    ];
+                }),
+            ];
 
             return response()->json([
                 'message' => 'Mantenimiento creado exitosamente.',
-                'results' => $maintenance,
+                'results' => $formattedData,
             ], 201);
         } catch (Exception $e) {
             DB::rollBack();
